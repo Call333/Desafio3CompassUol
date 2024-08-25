e1352cd1cefee057fbe3390da2477e0ecc54f1d0
Call333 <cam4@aluno.ifal.edu.br>
2024-08-25 12:41:54 -0300

    primeiro commit

---
+++ src/main/java/com/uol/compass/pb/ecommerce/ecommerce_desafio3/EcommerceDesafio3Application.java
@@ -0,0 +1,13 @@
+package com.uol.compass.pb.ecommerce.ecommerce_desafio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDesafio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDesafio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uol/compass/pb/ecommerce/ecommerce_desafio3/EcommerceDesafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uol.compass.pb.ecommerce.ecommerce_desafio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDesafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
