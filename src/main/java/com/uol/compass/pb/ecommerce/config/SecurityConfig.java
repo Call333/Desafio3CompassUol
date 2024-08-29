4a51f873bf5042b3e0aaa68d2234fa4f67f8c6c2
Call333 <cam4@aluno.ifal.edu.br>
2024-08-29 10:52:27 -0300

    Comecando a impl a seguranca

---
+++ src/main/java/com/uol/compass/pb/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.uol.compass.pb.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception{
+		return http.
+				authorizeHttpRequests(customizer -> {
+					customizer.anyRequest().authenticated();
+					
+				}).
+				httpBasic(Customizer.withDefaults()).
+				formLogin(Customizer.withDefaults()).
+				build();
+	}
+	
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails commonUser = User.builder()
+				.username("user")
+				.password("1234")
+				.roles("USER")
+				.build();
+		
+		UserDetails adminUser = User.builder()
+				.username("admin")
+				.password("@123")
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(commonUser, adminUser);
+	}
+}
